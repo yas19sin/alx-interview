d2f4727cd1150a4a324c27781062346b4c329da6
yas19sin <yas19sin@gmail.com>
2025-05-12 01:48:13 +0100

    0x03-log_parsing pycodestyle

--- 0x03-log_parsing/0-generator.py
+++ 0x03-log_parsing/0-generator.py
@@ -6,10 +6,13 @@ import datetime
 
 for i in range(10000):
     sleep(random.random())
-    sys.stdout.write("{:d}.{:d}.{:d}.{:d} - [{}] \"GET /260 HTTP/1.1\" {} {}\n".format(
-        random.randint(1, 255), random.randint(1, 255), random.randint(1, 255), random.randint(1, 255),
-        datetime.datetime.now(),
+    log_part1 = "{:d}.{:d}.{:d}.{:d} - [{}] ".format(
+        random.randint(1, 255), random.randint(1, 255),
+        random.randint(1, 255), random.randint(1, 255),
+        datetime.datetime.now())
+    log_part2 = "\"GET /260 HTTP/1.1\" {} {}\n".format(
         random.choice([200, 301, 400, 401, 403, 404, 405, 500]),
         random.randint(1, 1024)
-    ))
+    )
+    sys.stdout.write(log_part1 + log_part2)
     sys.stdout.flush()
--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -4,12 +4,14 @@ Log parsing script
 """
 import sys
 
+
 def print_stats(current_total_size, current_status_codes):
     """Prints the computed statistics"""
     print("File size: {}".format(current_total_size))
     for code in sorted(current_status_codes.keys()):
         if current_status_codes[code] > 0:
             print("{}: {}".format(code, current_status_codes[code]))
+
 
 total_size = 0
 status_codes = {
@@ -23,14 +25,14 @@ try:
     for line in sys.stdin:
         line = line.strip()
         parts = line.split()
-        
+
         is_line_valid_and_processed = False
         try:
             if not (len(parts) == 9 and
                     parts[4] == '"GET' and
                     parts[5] == '/260' and
                     parts[6] == 'HTTP/1.1"'):
-                pass 
+                pass
             else:
                 status_code = int(parts[7])
                 file_size = int(parts[8])
@@ -40,20 +42,21 @@ try:
                     status_codes[status_code] += 1
                 is_line_valid_and_processed = True
         except (ValueError, IndexError):
-            pass 
-        
+            pass
+
         if not is_line_valid_and_processed:
             continue
 
         num_valid_lines_processed += 1
 
-        if num_valid_lines_processed > 0 and num_valid_lines_processed % 10 == 0:
+        if num_valid_lines_processed > 0 and \
+                num_valid_lines_processed % 10 == 0:
             print_stats(total_size, status_codes)
 
 except KeyboardInterrupt:
     program_interrupted = True
     print_stats(total_size, status_codes)
-    raise 
+    raise
 
 finally:
     if not program_interrupted and \
